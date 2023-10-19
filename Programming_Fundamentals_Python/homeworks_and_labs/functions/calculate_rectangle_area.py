49bccc792aed994f1df9384d181bf23a8d1b6c6e
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-10-19 06:38:44 +0300

    Adding solutions to tasks from Functions lecture

---
+++ Programming_Fundamentals_Python/homeworks_and_labs/functions/calculate_rectangle_area.py
@@ -0,0 +1,4 @@
+result = lambda a, b: a * b
+width = int(input())
+height = int(input())
+print(result(width, height))
---
+++ Programming_Fundamentals_Python/homeworks_and_labs/functions/repeat_string.py
@@ -0,0 +1,5 @@
+repeat_string = lambda string, counter: string * counter
+
+string = input()
+counter = int(input())
+print(repeat_string(string, counter))
