09cf13308765c7fa7ac7afb5cb6cca7f0014d6c0
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-11-02 02:47:09 +0200

    Update heart_delivery.py

--- Programming_Fundamentals_Python/homeworks_and_labs/exam_preparation/heart_delivery.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/exam_preparation/heart_delivery.py
@@ -1,6 +1,5 @@
-neighbourhood = [int(s) for s in input().split('@')]
+houses = [int(s) for s in input().split('@')]
 
-already_had_valentines = []
 index = 0
 
 command_line = input()
@@ -8,18 +7,17 @@ while command_line != 'Love!':
     commands = command_line.split()
     length = int(commands[1])
     index += length
-    if index >= len(neighbourhood):
+    if index >= len(houses):
         index = 0
-    if neighbourhood[index] > 0:
-        neighbourhood[index] -= 2
-    if neighbourhood[index] == 0 and index in already_had_valentines:
+    if houses[index] == 0:
         print(f"Place {index} already had Valentine's day.")
-    elif neighbourhood[index] == 0:
-        print(f"Place {index} has Valentine's day.")
-        already_had_valentines.append(index)
+    else:
+        houses[index] -= 2
+        if houses[index] == 0:
+            print(f"Place {index} has Valentine's day.")
     command_line = input()
 
-not_celebrated_houses = [num for num in neighbourhood if num != 0]
+not_celebrated_houses = [num for num in houses if num != 0]
 
 print(f"Cupid's last position was {index}.")
 if len(not_celebrated_houses) == 0:
