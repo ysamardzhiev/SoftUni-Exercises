8b671020f0a7fc33429f03c7dbeae0d582debb6f
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-11-14 02:32:33 +0200

    Changed stock.py

--- Programming_Fundamentals_Python/homeworks_and_labs/dictionaries/stock.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/dictionaries/stock.py
@@ -1,5 +1,4 @@
 products_and_quantities = input().split()
-products = input().split()
 
 stock = {}
 
@@ -7,6 +6,8 @@ for i in range(0, len(products_and_quantities), 2):
     product = products_and_quantities[i]
     quantity = products_and_quantities[i + 1]
     stock[product] = int(quantity)
+
+products = input().split()
 
 for current_product in products:
     if current_product in stock:
