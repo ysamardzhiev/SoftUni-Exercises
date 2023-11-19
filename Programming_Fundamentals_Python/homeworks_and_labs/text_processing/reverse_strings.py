37d818dc2ce34ad35782eef37dd08036921b0ebe
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-11-19 02:20:19 +0200

    Update reverse_strings.py

--- Programming_Fundamentals_Python/homeworks_and_labs/text_processing/reverse_strings.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/text_processing/reverse_strings.py
@@ -1,6 +1,5 @@
 command = input()
 while command != 'end':
     word = command
-    reversed_word = [word[index] for index in range(len(word) - 1, -1, -1)]
-    print(f"{word} = {''.join(reversed_word)}")
+    print(f"{word} = {word[::-1]}")
     command = input()
