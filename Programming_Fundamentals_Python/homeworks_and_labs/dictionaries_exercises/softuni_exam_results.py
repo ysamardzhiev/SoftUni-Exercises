d0905318fe0e90afe0e7315d81ea5a19ac8b2716
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-11-26 23:31:24 +0200

    Update softuni_exam_results.py

--- Programming_Fundamentals_Python/homeworks_and_labs/dictionaries_exercises/softuni_exam_results.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/dictionaries_exercises/softuni_exam_results.py
@@ -1,35 +1,27 @@
-exam_statistics = {}
+users = {}
+languages = {}
 
 command = input()
 while command != 'exam finished':
     commands = command.split('-')
     username = commands[0]
-
-    if commands[1] == 'banned':
-        for language in exam_statistics.keys():
-            if username in exam_statistics[language].keys():
-                exam_statistics[language][username]['banned_status'] = True
+    if len(commands) == 2:
+        users.pop(username)
         command = input()
         continue
-
-    language, points = commands[1], int(commands[2])
-    if language not in exam_statistics.keys():
-        exam_statistics[language] = {}
-    if username not in exam_statistics[language].keys():
-        exam_statistics[language][username] = {'points': 0, 'submissions': 0, 'banned_status': False}
-    if points > exam_statistics[language][username]['points']:
-        exam_statistics[language][username]['points'] = points
-    exam_statistics[language][username]['submissions'] += 1
+    language = commands[1]
+    points = int(commands[2])
+    if username not in users:
+        users[username] = 0
+    if points > users[username]:
+        users[username] = points
+    if language not in languages:
+        languages[language] = 0
+    languages[language] += 1
     command = input()
-
 print('Results:')
-for language, users in exam_statistics.items():
-    for user in users.keys():
-        if not exam_statistics[language][user]["banned_status"]:
-            print(f'{user} | {exam_statistics[language][user]["points"]}')
+for user, points in users.items():
+    print(f'{user} | {points}')
 print('Submissions:')
-for language, users in exam_statistics.items():
-    total_submissions = 0
-    for user in users.keys():
-        total_submissions += exam_statistics[language][user]['submissions']
-    print(f'{language} - {total_submissions}')
+for language, submissions in languages.items():
+    print(f'{language} - {submissions}')
