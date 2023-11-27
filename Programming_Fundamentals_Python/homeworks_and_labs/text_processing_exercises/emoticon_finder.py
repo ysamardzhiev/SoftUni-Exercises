6c78623b5e393770945e513f38593f26eb481340
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-11-28 00:53:38 +0200

    Update emoticon_finder.py

--- Programming_Fundamentals_Python/homeworks_and_labs/text_processing_exercises/emoticon_finder.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/text_processing_exercises/emoticon_finder.py
@@ -1,9 +1,5 @@
 text = input()
 
-emoticons = []
 for i in range(len(text)):
     if text[i] == ':':
-        emoticons.append(text[i+1])
-
-for emoji in emoticons:
-    print(f':{emoji}')
+        print(f':{text[i+1]}')
