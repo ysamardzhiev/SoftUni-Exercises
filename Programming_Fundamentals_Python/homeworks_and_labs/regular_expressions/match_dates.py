367e506f575a72d851952ff6a4d9cea04be51d79
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-11-30 03:31:26 +0200

    Update match_dates.py

--- Programming_Fundamentals_Python/homeworks_and_labs/regular_expressions/match_dates.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/regular_expressions/match_dates.py
@@ -5,4 +5,5 @@ pattern = r"\b(\d{2})([\/.-])([A-Z][a-z]{2})\2(\d{4})\b"
 matches = re.findall(pattern, dates)
 
 for date in matches:
-    print(f'Day: {date[0]}, Month: {date[2]}, Year: {date[3]}')
+    day, month, year = date[0], date[2], date[3]
+    print(f'Day: {day}, Month: {month}, Year: {year}')
