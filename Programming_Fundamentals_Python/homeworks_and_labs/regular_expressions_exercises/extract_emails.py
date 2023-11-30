c7322b5da64886d75b1816ee03de7093cf40ba85
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-11-30 23:17:25 +0200

    Update extract_emails.py

--- Programming_Fundamentals_Python/homeworks_and_labs/regular_expressions_exercises/extract_emails.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/regular_expressions_exercises/extract_emails.py
@@ -2,7 +2,7 @@ import re
 
 text = input()
 
-valid_emails = re.finditer(r"\s(([a-z0-9]+[a-z0-9\\.\-\\_]*)@([a-z\-]+)(\.[a-z\-]+)+)\b", text)
+valid_emails = re.finditer(r"\s([a-z0-9]+[a-z0-9\\.\-\\_]*@[a-z\-]+(\.[a-z\-]+)+)\b", text)
 
 for email in valid_emails:
     print(email.group())
