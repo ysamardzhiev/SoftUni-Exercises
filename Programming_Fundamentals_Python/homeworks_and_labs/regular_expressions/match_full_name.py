251d20a0a588c12bd51e0b951cae0a92d0eb760a
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-11-30 02:50:36 +0200

    Update match_full_name.py

--- Programming_Fundamentals_Python/homeworks_and_labs/regular_expressions/match_full_name.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/regular_expressions/match_full_name.py
@@ -1,7 +1,7 @@
 import re
 
 names = input()
-pattern = '\\b[A-Z][a-z]{1,}\ [A-Z][a-z]{1,}\\b'
+pattern = r'\b[A-Z][a-z]{1,}\ [A-Z][a-z]{1,}\b'
 matches = re.findall(pattern, names)
 
 print(' '.join(matches))
