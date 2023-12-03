2fa050210f5fe21c8b3c196ebd0e091996a02a07
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-12-03 14:20:01 +0200

    Create string_game.py

---
+++ Programming_Fundamentals_Python/exams/final_exam_december_2023/string_game.py
@@ -0,0 +1,38 @@
+string = input()
+
+command = input()
+while command != 'Done':
+    commands = command.split()
+    action = commands[0]
+    if action == 'Change':
+        char, replacement = commands[1], commands[2]
+        string = string.replace(char, replacement)
+        print(string)
+    elif action == 'Includes':
+        substring = commands[1]
+        if substring in string:
+            print('True')
+        else:
+            print('False')
+    elif action == 'End':
+        substring = commands[1]
+        if substring.endswith(substring):
+            print('True')
+        else:
+            print('False')
+    elif action == 'Uppercase':
+        string = string.upper()
+        print(string)
+    elif action == 'FindIndex':
+        char = commands[1]
+        char_index = string.index(char)
+        print(char_index)
+    elif action == 'Cut':
+        start_index, count = int(commands[1]), int(commands[2])
+        cut_chars = ''
+        for _ in range(count):
+            cut_chars += string[start_index]
+            start_index += 1
+        string = cut_chars
+        print(string)
+    command = input()
