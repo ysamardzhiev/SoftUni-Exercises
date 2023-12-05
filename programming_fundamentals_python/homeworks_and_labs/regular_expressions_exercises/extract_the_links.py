54157b3a7388c9291a544ab202f21af58c38f00f
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-12-05 23:42:59 +0200

    Delete programming_fundamentals_python directory

--- programming_fundamentals_python/homeworks_and_labs/regular_expressions_exercises/extract_the_links.py
+++
@@ -1,8 +0,0 @@
-import re
-
-text = input()
-while text:
-    match = re.findall(r"www\.[a-zA-Z0-9\]-]+\.[a-z\\.]+", text)
-    if match:
-        print(''.join(match))
-    text = input()
--- programming_fundamentals_python/homeworks_and_labs/text_processing_exercises/replace_repeating_chars.py
+++
@@ -1,9 +0,0 @@
-text = input()
-
-final_text = ''
-for i in range(len(text)):
-    if i == 0:
-        final_text += text[i]
-    elif text[i] != text[i-1]:
-        final_text += text[i]
-print(final_text)
