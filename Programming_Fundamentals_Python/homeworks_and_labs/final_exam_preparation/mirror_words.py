de78d2bc7ee5a5d428bc3adb70038342a3c21a85
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-12-05 22:45:28 +0200

    Update mirror_words.py

--- Programming_Fundamentals_Python/homeworks_and_labs/final_exam_preparation/mirror_words.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/final_exam_preparation/mirror_words.py
@@ -1,10 +1,29 @@
 import re
 
+
+def check_validity(text):
+    valid_pairs = []
+
+    matches = re.finditer(r"(@|#)([A-Za-z]{3,})\1{2}([A-Za-z]{3,})\1", text)
+    for word in matches:
+        valid_pairs.append((word.group(2), word.group(3)))
+    return valid_pairs
+
+
 text = input()
 
-matches = re.findall(r"(@|#)([a-zA-Z]{3,})\1", text)
+mirror_words = []
 
-if matches:
-    print(f"{len(matches)} word pairs found!")
+if check_validity(text):
+    print(f"{len(check_validity(text))} word pairs found!")
+    for word1, word2 in check_validity(text):
+        if word1 == word2[::-1]:
+            mirror_words.append(f'{word1} <=> {word2}')
+else:
+    print("No word pairs found!")
+
+if mirror_words:
     print('The mirror words are:')
-    print(matches)
+    print(', '.join(mirror_words))
+else:
+    print('No mirror words!')
