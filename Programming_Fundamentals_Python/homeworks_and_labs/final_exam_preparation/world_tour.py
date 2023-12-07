9217e243d53b0e504f42c7e5441e3fb797109d6a
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2023-12-07 02:37:10 +0200

    Update world_tour.py

--- Programming_Fundamentals_Python/homeworks_and_labs/final_exam_preparation/world_tour.py
+++ Programming_Fundamentals_Python/homeworks_and_labs/final_exam_preparation/world_tour.py
@@ -7,16 +7,14 @@ while command != 'Travel':
         index, string = int(commands[1]), commands[2]
         if 0 <= index < len(stops):
             stops = stops[:index] + string + stops[index:]
-        print(stops)
     elif commands[0] == 'Remove Stop':
         start_index, end_index = int(commands[1]), int(commands[2])
         if 0 <= start_index < len(stops) and 0 <= end_index < len(stops):
             stops = stops[:start_index] + stops[end_index + 1:]
-        print(stops)
     elif commands[0] == 'Switch':
         old_string, new_string = commands[1], commands[2]
         if old_string in stops:
             stops = stops.replace(old_string, new_string)
-        print(stops)
+    print(stops)
     command = input()
 print(f"Ready for world tour! Planned stops: {stops}")
