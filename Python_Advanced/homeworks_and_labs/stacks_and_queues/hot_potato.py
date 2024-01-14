e55873ca2ce84905e56ac672855bbfb00fa3793c
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-15 00:16:50 +0200

    Update hot_potato.py

--- Python_Advanced/homeworks_and_labs/stacks_and_queues/hot_potato.py
+++ Python_Advanced/homeworks_and_labs/stacks_and_queues/hot_potato.py
@@ -1,19 +1,11 @@
 from collections import deque
 
 players = deque(input().split())
-n_toss = int(input())
-
-current_toss = 0
+n_toss = int(input()) - 1
 
 while len(players) != 1:
-    for player in players:
-        current_toss += 1
-        if current_toss == n_toss:
-            print(f'Removed {player}')
-            players.remove(player)
-            current_toss = 0
-        else:
-            players.remove(player)
-            players.append(player)
-        break
-print(f'Last is {players.pop()}')
+    players.rotate(-n_toss)
+    removed_player = players.popleft()
+    print(f'Removed {removed_player}')
+last_player = players.popleft()
+print(f'Last is {last_player}')
