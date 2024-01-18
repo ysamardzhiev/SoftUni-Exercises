0e5792c7eb0829410df840425d2b4ce3c07764cd
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-19 01:21:04 +0200

    Update stacked_queries.py

--- Python_Advanced/homeworks_and_labs/stacks_and_queues_exercises/stacked_queries.py
+++ Python_Advanced/homeworks_and_labs/stacks_and_queues_exercises/stacked_queries.py
@@ -2,21 +2,22 @@ from collections import deque
 
 n = int(input())
 
-stack = deque()
+numbers = deque()
 
 for _ in range(n):
     query = input().split()
-    if query[0] == '1':
-        stack.append(int(query[1]))
-    elif query[0] == '2':
-        if stack:
-            stack.pop()
-    elif query[0] == '3':
-        if stack:
-            print(max(stack))
-    elif query[0] == '4':
-        if stack:
-            print(min(stack))
-stack = [str(number) for number in stack]
-stack.reverse()
-print(', '.join(stack))
+    command = query[0]
+    if command == '1':
+        numbers.append(int(query[1]))
+    elif command == '2':
+        if numbers:
+            numbers.pop()
+    elif command == '3':
+        if numbers:
+            print(max(numbers))
+    elif command == '4':
+        if numbers:
+            print(min(numbers))
+numbers = [str(number) for number in numbers]
+numbers.reverse()
+print(', '.join(numbers))
