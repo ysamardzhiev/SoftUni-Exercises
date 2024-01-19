4b6f934d6cda2dda6d6337b7ed05ce8496de90e4
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-19 03:22:53 +0200

    Update fast_food.py

--- Python_Advanced/homeworks_and_labs/stacks_and_queues_exercises/fast_food.py
+++ Python_Advanced/homeworks_and_labs/stacks_and_queues_exercises/fast_food.py
@@ -5,12 +5,11 @@ food_quantity = int(input())
 orders = deque([int(order) for order in input().split()])
 biggest_order = max(orders)
 
-while orders:
-    current_order = orders.popleft()
-    if current_order <= food_quantity:
-        food_quantity -= current_order
+for order in orders.copy():
+    if order <= food_quantity:
+        food_quantity -= order
+        orders.popleft()
     else:
-        orders.appendleft(current_order)
         break
 print(biggest_order)
 if orders:
