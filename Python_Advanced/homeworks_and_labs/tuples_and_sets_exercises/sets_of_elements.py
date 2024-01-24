e6811e5ad2cc6e67d4e9eadda9ebee98628b7772
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-25 01:09:05 +0200

    Update sets_of_elements.py

--- Python_Advanced/homeworks_and_labs/tuples_and_sets_exercises/sets_of_elements.py
+++ Python_Advanced/homeworks_and_labs/tuples_and_sets_exercises/sets_of_elements.py
@@ -2,4 +2,4 @@ n, m = [int(x) for x in input().split()]
 
 first_set = set(input() for _ in range(n))
 second_set = set(input() for _ in range(m))
-print(*(first_set.intersection(second_set)), sep='\n')
+print(*first_set.intersection(second_set), sep='\n')
