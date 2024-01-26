1a5b164a8875317895d50fc665c5b105662e220c
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-26 02:33:06 +0200

    Create battle_of_names.py

---
+++ Python_Advanced/homeworks_and_labs/tuples_and_sets_exercises/battle_of_names.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+odd_set = set()
+even_set = set()
+
+for row in range(1, n + 1):
+    name = input()
+    ascii_sum = 0
+    for letter in name:
+        ascii_sum += ord(letter)
+
+    final_result = ascii_sum // row
+    if final_result % 2:
+        odd_set.add(final_result)
+    else:
+        even_set.add(final_result)
+
+odd_numbers_sum = sum(odd_set)
+even_numbers_sum = sum(even_set)
+if odd_numbers_sum == even_numbers_sum:
+    print(*odd_set.union(even_set), sep=', ')
+elif even_numbers_sum > odd_numbers_sum:
+    print(*odd_set.symmetric_difference(even_set), sep=', ')
+else:
+    print(*odd_set.difference(even_set), sep=', ')
