df198fc093e5e27064c3be1db5d30dcd829c4b9d
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-28 01:34:12 +0200

    Update honey.py

--- Python_Advanced/homeworks_and_labs/stacks_queues_tuples_and_sets/honey.py
+++ Python_Advanced/homeworks_and_labs/stacks_queues_tuples_and_sets/honey.py
@@ -3,7 +3,7 @@ from collections import deque
 bees = deque(int(num) for num in input().split())
 nectar = deque(int(num) for num in input().split())
 symbols = deque(input().split())
-hive = deque()
+total_honey = 0
 
 while bees and nectar:
     bee_value, nectar_value = bees.popleft(), nectar.pop()
@@ -11,10 +11,10 @@ while bees and nectar:
         symbol = symbols.popleft()
         if symbol == '/' and nectar_value == 0:
             continue
-        hive.append(abs(eval(f'{bee_value} {symbol} {nectar_value}')))
+        total_honey += (abs(eval(f'{bee_value} {symbol} {nectar_value}')))
     else:
         bees.appendleft(bee_value)
-print(f'Total honey made: {sum(hive)}')
+print(f'Total honey made: {total_honey}')
 if bees:
     print(f'Bees left: {", ".join(str(num) for num in bees)}')
 elif nectar:
