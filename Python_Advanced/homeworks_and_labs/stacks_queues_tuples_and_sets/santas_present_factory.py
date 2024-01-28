51aaf69f12ff0595a0a668f24e1986d4460e873e
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-28 03:02:18 +0200

    Update santas_present_factory.py

--- Python_Advanced/homeworks_and_labs/stacks_queues_tuples_and_sets/santas_present_factory.py
+++ Python_Advanced/homeworks_and_labs/stacks_queues_tuples_and_sets/santas_present_factory.py
@@ -1,17 +1,17 @@
 from collections import deque
 
-material_boxes = [int(x) for x in input().split()]
+materials = [int(x) for x in input().split()]
 magic_levels = deque(int(x) for x in input().split())
 presents = {150: 'Doll', 250: 'Wooden train', 300: 'Teddy bear', 400: 'Bicycle'}
 
 crafted_presents = {}
 
-while material_boxes and magic_levels:
-    material_value, magic_level = material_boxes.pop(), magic_levels.popleft()
+while materials and magic_levels:
+    material_value, magic_level = materials.pop(), magic_levels.popleft()
     if material_value == 0 and magic_level == 0:
         continue
     elif magic_level == 0:
-        material_boxes.append(material_value)
+        materials.append(material_value)
         continue
     elif material_value == 0:
         magic_levels.appendleft(magic_level)
@@ -25,17 +25,17 @@ while material_boxes and magic_levels:
         crafted_presents[toy] += 1
     elif total_magic_level < 0:
         total_magic_level = material_value + magic_level
-        material_boxes.append(total_magic_level)
+        materials.append(total_magic_level)
     elif total_magic_level > 0:
-        material_boxes.append(material_value + 15)
+        materials.append(material_value + 15)
 
 if 'Doll' and 'Wooden train' in crafted_presents or 'Teddy bear' and 'Bicycle' in crafted_presents:
     print("The presents are crafted! Merry Christmas!")
 else:
     print("No presents this Christmas!")
 
-if material_boxes:
-    print(f'Materials left: {", ".join(str(x) for x in material_boxes[::-1])}')
+if materials:
+    print(f'Materials left: {", ".join(str(x) for x in materials[::-1])}')
 elif magic_levels:
     print(f'Magic left: {", ".join(str(x) for x in magic_levels)}')
 
