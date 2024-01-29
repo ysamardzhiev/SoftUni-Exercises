46cff2206ebf96e32c513ab302c9880b4b361685
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-29 04:38:04 +0200

    Create milkshakes.py

---
+++ Python_Advanced/homeworks_and_labs/stacks_queues_tuples_and_sets/milkshakes.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+chocolates = [int(x) for x in input().split(', ')]
+cups_of_milk = deque([int(x) for x in input().split(', ')])
+
+milkshakes_count = 0
+
+while chocolates and cups_of_milk and milkshakes_count < 5:
+    chocolate = chocolates.pop() if cups_of_milk[0] > 0 or chocolates[-1] <= 0 else 0
+    cup_of_milk = cups_of_milk.popleft() if chocolate > 0 or cups_of_milk[0] <= 0 else 0
+
+    if cup_of_milk <= 0:
+        continue
+
+    if chocolate == cup_of_milk:
+        milkshakes_count += 1
+    else:
+        cups_of_milk.append(cup_of_milk)
+        chocolates.append(chocolate - 5)
+
+print("Great! You made all the chocolate milkshakes needed!") if milkshakes_count == 5 else print("Not enough milkshakes.")
+print(f'Chocolate: {", ".join(str(x) for x in chocolates)}') if chocolates else print("Chocolate: empty")
+print(f'Milk: {", ".join(str(x) for x in cups_of_milk)}') if cups_of_milk else print("Milk: empty")
