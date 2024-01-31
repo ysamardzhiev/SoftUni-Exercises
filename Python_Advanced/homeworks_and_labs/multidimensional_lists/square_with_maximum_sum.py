0a1b3f0f82d1bf68bf2e8443310161062ca6c332
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-31 05:01:51 +0200

    Update square_with_maximum_sum.py

--- Python_Advanced/homeworks_and_labs/multidimensional_lists/square_with_maximum_sum.py
+++ Python_Advanced/homeworks_and_labs/multidimensional_lists/square_with_maximum_sum.py
@@ -16,6 +16,6 @@ for row_index in range(rows - 1):
             biggest_sub_matrix = [[matrix[row_index][col_index], matrix[row_index][col_index+1]],
                                  [matrix[row_index+1][col_index], matrix[row_index+1][col_index+1]]]
 
-for row in biggest_sub_matrix:
-    print(*row)
+print(*biggest_sub_matrix[0])
+print(*biggest_sub_matrix[1])
 print(biggest_sum)
