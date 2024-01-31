33b7a6e6ff2d87e58db98526e487a77c8a28505a
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-01-31 02:42:30 +0200

    Update primary_diagonal.py

--- Python_Advanced/homeworks_and_labs/multidimensional_lists/primary_diagonal.py
+++ Python_Advanced/homeworks_and_labs/multidimensional_lists/primary_diagonal.py
@@ -1,11 +1,11 @@
 rows = int(input())
 
-matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 diagonal_sum = 0
 
-for row_index in range(len(matrix)):
-    for col_index in range(len(matrix)):
+for row_index in range(rows):
+    row_data = [int(x) for x in input().split()]
+    for col_index in range(rows):
         if row_index == col_index:
-            diagonal_sum += matrix[row_index][col_index]
+            diagonal_sum += row_data[col_index]
             break
 print(diagonal_sum)
