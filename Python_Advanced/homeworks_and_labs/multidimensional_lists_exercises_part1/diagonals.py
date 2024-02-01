2eeda1fc0679aa3df719f46378a8e7a9509227dd
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-02 01:05:00 +0200

    Update diagonals.py

--- Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part1/diagonals.py
+++ Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part1/diagonals.py
@@ -5,11 +5,7 @@ diagonals = [[], []]
 
 for row_index in range(len(matrix)):
     diagonals[0].append(matrix[row_index][row_index])
-
-row_index = 0
-for col_index in range(len(matrix) - 1, -1, -1):
-    diagonals[1].append(matrix[row_index][col_index])
-    row_index += 1
+    diagonals[1].append(matrix[row_index][len(matrix) - row_index - 1])
 
 print(f'Primary diagonal: {", ".join(str(x) for x in diagonals[0])}. Sum: {sum(diagonals[0])}')
 print(f'Secondary diagonal: {", ".join(str(x) for x in diagonals[1])}. Sum: {sum(diagonals[1])}')
