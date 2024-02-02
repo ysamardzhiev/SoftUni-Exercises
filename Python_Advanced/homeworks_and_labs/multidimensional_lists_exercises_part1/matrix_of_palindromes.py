a4b6263da9cea0d318f433a8e8fd3aaf30ce0628
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-02 04:29:53 +0200

    Update matrix_of_palindromes.py

--- Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part1/matrix_of_palindromes.py
+++ Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part1/matrix_of_palindromes.py
@@ -3,11 +3,8 @@ import string
 rows, cols = [int(x) for x in input().split()]
 
 alphabet = string.ascii_lowercase
-matrix = []
 
 for row_index in range(rows):
-    matrix.append([])
     for col_index in range(cols):
-        matrix[row_index].append(f'{alphabet[row_index]}{alphabet[row_index + col_index]}{alphabet[row_index]}')
-
-[print(*row) for row in matrix]
+        print(f'{alphabet[row_index]}{alphabet[row_index + col_index]}{alphabet[row_index]}', end=' ')
+    print()
