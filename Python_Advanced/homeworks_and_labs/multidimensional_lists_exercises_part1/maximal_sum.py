41f784c75f76af5062160a7c14d73a6f849420d3
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-02 03:54:35 +0200

    Update maximal_sum.py

--- Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part1/maximal_sum.py
+++ Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part1/maximal_sum.py
@@ -7,21 +7,13 @@ biggest_sub_matrix = []
 
 for row_index in range(rows - 2):
     for col_index in range(cols - 2):
-        first_el = matrix[row_index][col_index]
-        second_el = matrix[row_index][col_index + 1]
-        third_el = matrix[row_index][col_index + 2]
-        fourth_el = matrix[row_index + 1][col_index]
-        fifth_el = matrix[row_index + 1][col_index + 1]
-        sixth_el = matrix[row_index + 1][col_index + 2]
-        seventh_el = matrix[row_index + 2][col_index]
-        eighth_el = matrix[row_index + 2][col_index + 1]
-        ninth_el = matrix[row_index + 2][col_index + 2]
-        sub_matrix_sum = first_el + second_el + third_el \
-                         + fourth_el + fifth_el + sixth_el \
-                         + seventh_el + eighth_el + ninth_el
+        first_row = matrix[row_index][col_index:col_index + 3]
+        second_row = matrix[row_index + 1][col_index:col_index + 3]
+        third_row = matrix[row_index + 2][col_index:col_index + 3]
+        sub_matrix_sum = sum(first_row + second_row + third_row)
         if sub_matrix_sum > biggest_sum:
             biggest_sum = sub_matrix_sum
-            biggest_sub_matrix = [[first_el, second_el, third_el], [fourth_el, fifth_el, sixth_el], [seventh_el, eighth_el, ninth_el]]
+            biggest_sub_matrix = [first_row, second_row, third_row]
 
 print(f'Sum = {biggest_sum}')
 print(*biggest_sub_matrix[0])
