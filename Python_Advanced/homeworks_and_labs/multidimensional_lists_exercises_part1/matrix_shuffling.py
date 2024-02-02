a6b352e86c439c51239d68fe7ed576b1541aa0b0
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-02 13:23:12 +0200

    Update matrix_shuffling.py

--- Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part1/matrix_shuffling.py
+++ Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part1/matrix_shuffling.py
@@ -5,15 +5,12 @@ matrix = [input().split() for _ in range(rows)]
 command = input()
 while command != 'END':
     commands = command.split()
-    if commands[0] == 'swap':
+    if commands[0] == 'swap' and len(commands) == 5:
         coordinates = [int(x) for x in commands[1:]]
-        if len(coordinates) == 4:
-            row1, col1, row2, col2 = coordinates
-            if row1 < rows and row2 < rows and col1 < cols and col2 < cols:
-                matrix[row1][col1], matrix[row2][col2] = matrix[row2][col2], matrix[row1][col1]
-                [print(*row) for row in matrix]
-            else:
-                print('Invalid input!')
+        row1, col1, row2, col2 = coordinates
+        if 0 <= row1 < rows and 0 <= row2 < rows and 0 <= col1 < cols and 0 <= col2 < cols:
+            matrix[row1][col1], matrix[row2][col2] = matrix[row2][col2], matrix[row1][col1]
+            [print(*row) for row in matrix]
         else:
             print('Invalid input!')
     else:
