c75db31bafc4960e0b9a7928ab90051cd4605c18
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-02 02:01:34 +0200

    Create 2x2_squares_in_matrix.py

---
+++ Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part1/2x2_squares_in_matrix.py
@@ -0,0 +1,15 @@
+rows, cols = [int(x) for x in input().split()]
+
+matrix = [[el for el in input().split()] for _ in range(rows)]
+
+found_squares = 0
+
+for row_index in range(rows - 1):
+    for col_index in range(cols - 1):
+        first_el = matrix[row_index][col_index]
+        second_el = matrix[row_index][col_index + 1]
+        third_el = matrix[row_index + 1][col_index]
+        fourth_el = matrix[row_index + 1][col_index + 1]
+        if first_el == second_el == third_el == fourth_el:
+            found_squares += 1
+print(found_squares)
