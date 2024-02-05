1a8a7e214d940c2d467a465a2f84db3052740ef5
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-05 03:31:17 +0200

    Create range_day.py

---
+++ Python_Advanced/homeworks_and_labs/multidimensional_lists_exercises_part_2/range_day.py
@@ -0,0 +1,60 @@
+size = 5
+
+matrix = []
+
+shooting_pos = []
+
+total_targets = 0
+found_targets = []
+
+for row_index in range(size):
+    matrix.append(input().split())
+    if 'A' in matrix[row_index]:
+        shooting_pos = [row_index, matrix[row_index].index('A')]
+        matrix[row_index][shooting_pos[1]] = '.'
+    if 'x' in matrix[row_index]:
+        total_targets += matrix[row_index].count('x')
+
+directions = {
+    'up': (-1, 0),
+    'down': (1, 0),
+    'left': (0, -1),
+    'right': (0, 1),
+}
+
+n = int(input())
+
+for _ in range(n):
+    commands = input().split()
+    type_of_command, direction = commands[0], commands[1]
+    row, col = shooting_pos[0], shooting_pos[1]
+    x, y = directions[direction][0], directions[direction][1]
+
+    if type_of_command == 'move':
+        steps = int(commands[2])
+        for _ in range(steps):
+            if not (0 <= row + x < size and 0 <= col + y < size):
+                break
+            row += x
+            col += y
+        if matrix[row][col] != '.':
+            row, col = shooting_pos
+        shooting_pos = [row, col]
+
+    elif type_of_command == 'shoot':
+        while 0 <= row < size and 0 <= col < size:
+            if matrix[row][col] == 'x':
+                found_targets.append([row, col])
+                total_targets -= 1
+                matrix[row][col] = '.'
+                break
+            row += x
+            col += y
+
+        if not total_targets:
+            print(f"Training completed! All {len(found_targets)} targets hit.")
+            break
+else:
+    print(f'Training not completed! {total_targets} targets left.')
+
+print(*found_targets, sep='\n')
