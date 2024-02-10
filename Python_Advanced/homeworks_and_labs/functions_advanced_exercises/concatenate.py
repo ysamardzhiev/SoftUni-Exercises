0c271799ce72816266134348c60e988894b7475c
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-10 02:35:01 +0200

    Create concatenate.py

---
+++ Python_Advanced/homeworks_and_labs/functions_advanced_exercises/concatenate.py
@@ -0,0 +1,15 @@
+def concatenate(*args, **kwargs):
+    result = ''
+
+    for el in args:
+        result += el
+
+    for key, string in kwargs.items():
+        if key in result:
+            result = result.replace(key, string)
+            
+    return ''.join(result)
+
+
+print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))
+print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
