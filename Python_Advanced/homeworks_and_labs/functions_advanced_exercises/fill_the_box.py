7c4a24883dd695474e9b201fb3b952fa47130a42
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-11 03:09:45 +0200

    Create fill_the_box.py

---
+++ Python_Advanced/homeworks_and_labs/functions_advanced_exercises/fill_the_box.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+
+def fill_the_box(*args):
+    elements = deque(args)
+    box_size = elements.popleft() * elements.popleft() * elements.popleft()
+
+    total_cubes = 0
+
+    while elements:
+        current_element = elements.popleft()
+        if current_element == 'Finish':
+            break
+
+        total_cubes += current_element
+
+    if total_cubes >= box_size:
+        return f'No more free space! You have {total_cubes - box_size} more cubes.'
+    return f'There is free space in the box. You could put {box_size - total_cubes} more cubes.'
+
+
+print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
+print(fill_the_box(5, 5, 2, 40, 11, 7, 3, 1, 5, "Finish"))
+print(fill_the_box(10, 10, 10, 40, "Finish", 2, 15, 30))
