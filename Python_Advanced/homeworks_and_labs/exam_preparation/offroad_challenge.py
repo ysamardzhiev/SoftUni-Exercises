6597ef184bf50ec41d44accb26961256932af7ef
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-16 03:15:49 +0200

    Update offroad_challenge.py

--- Python_Advanced/homeworks_and_labs/exam_preparation/offroad_challenge.py
+++ Python_Advanced/homeworks_and_labs/exam_preparation/offroad_challenge.py
@@ -9,11 +9,9 @@ reached_altitudes = []
 for n in range(1, len(fuel_quantity) + 1):
     fuel = fuel_quantity.pop()
     consum_index = consumption_indexes.popleft()
-
-    result = fuel - consum_index
     fuel_needed = needed_fuel.popleft()
 
-    if result < fuel_needed:
+    if fuel - consum_index < fuel_needed:
         print(f"John did not reach: Altitude {n}")
         print("John failed to reach the top.")
         print(f"Reached altitudes: {', '.join(reached_altitudes)}") if reached_altitudes else print("John didn't reach any altitude.")
