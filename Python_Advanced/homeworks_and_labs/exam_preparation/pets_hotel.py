6d909f16add776876619a16bc4f9eb89d579a5dd
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-17 05:26:19 +0200

    Create pets_hotel.py

---
+++ Python_Advanced/homeworks_and_labs/exam_preparation/pets_hotel.py
@@ -0,0 +1,35 @@
+def accommodate_new_pets(capacity: int, max_weight: float, *pets):
+    accommodated_pets = {}
+    result = ''
+
+    for pet, weight in pets:
+        if not capacity:
+            result += "You did not manage to accommodate all pets!\n"
+            break
+        if weight > max_weight:
+            continue
+
+        accommodated_pets.setdefault(pet, 0)
+        accommodated_pets[pet] += 1
+        capacity -= 1
+    else:
+        result += f"All pets are accommodated! Available capacity: {capacity}.\n"
+
+    result += 'Accommodated pets:\n'
+
+    for pet, quantity in sorted(accommodated_pets.items()):
+        result += f'{pet}: {quantity}\n'
+
+    return result[:-1]
+
+
+
+print(accommodate_new_pets(
+    2,
+    15.0,
+    ("dog", 10.0),
+    ("cat", 5.8),
+    ("cat", 2.7),
+))
+
+
