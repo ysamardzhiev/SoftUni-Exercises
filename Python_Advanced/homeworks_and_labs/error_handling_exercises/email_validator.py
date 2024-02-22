f1316bfa5f95b94f477d5478e84b47e9a36763f4
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-23 00:39:24 +0200

    Update email_validator.py

--- Python_Advanced/homeworks_and_labs/error_handling_exercises/email_validator.py
+++ Python_Advanced/homeworks_and_labs/error_handling_exercises/email_validator.py
@@ -25,7 +25,7 @@ while command != 'End':
         raise NameTooShortError("Name must be more than 4 characters")
     elif '@' not in email:
         raise MustContainAtSymbolError("Email must contain @")
-    elif email.split('.')[1] not in ALLOWED_DOMAINS:
+    elif email.split('.')[-1] not in ALLOWED_DOMAINS:
         raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
     elif email.count('@') > 1:
         raise SymbolRepetition("Email must contain only one @")
