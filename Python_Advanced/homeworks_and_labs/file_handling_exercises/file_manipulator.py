0e0b8a79dfcc0cd90e35fff9a05271cd00ea9336
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-29 12:34:48 +0200

    Create file_manipulator.py

---
+++ Python_Advanced/homeworks_and_labs/file_handling_exercises/file_manipulator.py
@@ -0,0 +1,31 @@
+import os
+
+command = input()
+while command != 'End':
+    action, file_name, *additional = command.split('-')
+    path = os.path.join('files', file_name)
+
+    if action == 'Create':
+        with open(path, 'w') as file:
+            file.write('')
+    elif action == 'Add':
+        with open(path, 'a') as file:
+            file.write(f"{' '.join(additional)}\n")
+    elif action == 'Replace':
+        if os.path.exists(path):
+            old_str, new_str = additional[0], additional[1]
+            with open(path, 'r') as file:
+                content = file.read()
+
+            with open(path, 'w') as file:
+                content = content.replace(old_str, new_str)
+                file.write(content)
+        else:
+            print("An error occurred")
+    elif action == 'Delete':
+        try:
+            os.remove(path)
+        except FileNotFoundError:
+            print("An error occurred")
+
+    command = input()
