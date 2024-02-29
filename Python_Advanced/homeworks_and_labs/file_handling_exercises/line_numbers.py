81bf479a7b5bed7bf800bdc8753a99d9b8ae0b61
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-02-29 11:25:26 +0200

    Update line_numbers.py

--- Python_Advanced/homeworks_and_labs/file_handling_exercises/line_numbers.py
+++ Python_Advanced/homeworks_and_labs/file_handling_exercises/line_numbers.py
@@ -7,15 +7,15 @@ with open(text_path, 'r') as file:
     text = file.readlines()
 
 with open(output_path, 'w') as file:
-    current_row = 0
-    for line in text:
+    current_line = 0
+    for row in text:
         letters, punc_marks = 0, 0
-        current_row += 1
-        for char in line.strip():
+        current_line += 1
+        for char in row.strip():
             if char.isspace():
                 continue
             elif char.isalpha():
                 letters += 1
             else:
                 punc_marks += 1
-        file.write(f"Line {current_row}: {line.strip()} ({letters})({punc_marks})\n")
+        file.write(f"Line {current_line}: {row.strip()} ({letters})({punc_marks})\n")
