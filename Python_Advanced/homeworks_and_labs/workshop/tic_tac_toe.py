55e95b78ed2272502be2331bc0bf84638f59ecf2
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-03-10 02:09:20 +0200

    Create tic_tac_toe.py

---
+++ Python_Advanced/homeworks_and_labs/workshop/tic_tac_toe.py
@@ -0,0 +1,153 @@
+class InvalidNumber(Exception):
+    pass
+
+
+class InvalidSymbol(Exception):
+    pass
+
+
+class PositionNotAvailable(Exception):
+    pass
+
+
+BOARD_SIZE = 3
+WINNING_MOVES = 3
+
+NUM_OPERATIONS = {
+    1: (0, 0),
+    2: (0, 1),
+    3: (0, 2),
+    4: (1, 0),
+    5: (1, 1),
+    6: (1, 2),
+    7: (2, 0),
+    8: (2, 1),
+    9: (2, 2),
+}
+
+DIRECTIONS = {
+    'up': (-1, 0),
+    'left': (0, -1),
+    'first_diagonal': (-1, -1),
+    'second_diagonal': (-1, 1),
+}
+
+
+def traverse_direction(row, col, matrix, searched_symbol, coordinates,opposite=False):
+    count = 0
+    for _ in range(WINNING_MOVES):
+        if not (0 <= row < BOARD_SIZE and 0 <= col < BOARD_SIZE) or matrix[row][col] != searched_symbol:
+            break
+
+        count += 1
+        if opposite:
+            row -= coordinates[0]
+            col -= coordinates[1]
+        else:
+            row += coordinates[0]
+            col += coordinates[1]
+
+    return count
+
+
+def winning_position(game_board, row, col, symbol):
+    for direction, data in DIRECTIONS.items():
+        row_index, col_index = row + data[0], col + data[1]
+        row_opposite, col_opposite = row - data[0], col - data[1]
+
+        dir_count = traverse_direction(row_index, col_index, game_board, symbol, data)
+        reverse_dir_count = traverse_direction(row_opposite, col_opposite, game_board, symbol, data, opposite=True)
+
+        if dir_count + reverse_dir_count + 1 == WINNING_MOVES:
+            return True
+    return False
+
+
+def print_user_interface(first_player):
+    print('This is the numeration of the board:')
+    print(f'|  1  |  2  |  3  |')
+    print(f'|  4  |  5  |  6  |')
+    print(f'|  7  |  8  |  9  |')
+    print(f'{first_player} starts first!')
+
+
+def print_board(game_board):
+    for row in game_board:
+        print('|  ', end='')
+        print('  |  '.join(row), end='')
+        print('  |')
+
+
+def is_valid_number(player_choice):
+    if player_choice in NUM_OPERATIONS:
+        return NUM_OPERATIONS[player_choice]
+    raise InvalidNumber
+
+
+def is_symbol_valid(player_symbol):
+    if player_symbol == 'X' or player_symbol == 'O':
+        return 'O' if player_symbol == 'X' else 'X'
+    raise InvalidSymbol
+
+
+def is_position_available(game_board, row_index, col_index):
+    symbol = game_board[row_index][col_index]
+    if symbol == 'X' or symbol == 'O':
+        raise PositionNotAvailable
+    return True
+
+
+def is_board_full(turns_count):
+    return BOARD_SIZE * BOARD_SIZE <= turns_count
+
+
+player_one = input('Enter player 1 name: ')
+player_two = input('Enter player 2 name: ')
+
+board = [[' ' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+
+players = {player_one: None, player_two: None}
+
+turns = 1
+
+while True:
+    if turns == 1:
+        player1_symbol = input(f'{player_one} would you like to play with "X" or "O": ').capitalize()
+
+        try:
+            player2_symbol = is_symbol_valid(player1_symbol)
+        except InvalidSymbol:
+            print(f'Please choose between "X" and "O"!')
+            continue
+
+        players[player_one] = player1_symbol
+        players[player_two] = player2_symbol
+        print_user_interface(player_one)
+
+    current_player = player_one if turns % 2 else player_two
+    current_symbol = players[current_player]
+
+    try:
+        player_pos = int(input(f'{current_player} choose a free position from 1 to 9: '))
+        row, col = is_valid_number(player_pos)
+
+        if is_position_available(board, row, col):
+            board[row][col] = current_symbol
+
+        if is_board_full(turns):
+            print('The board is full! DRAW!')
+            exit(0)
+
+        if winning_position(board, row, col, current_symbol):
+            print(f'The game is over! The winner is {current_player}!')
+            exit(0)
+
+    except (ValueError, InvalidNumber):
+        print('Please enter a number from 1 to 9!')
+        continue
+    except PositionNotAvailable:
+        print('This position is already taken! Choose another one.')
+        continue
+
+    print_board(board)
+    turns += 1
