12c2bd09a5ab504bf017f26bb9e11d39efefaeb6
Yoan Samardzhiev <146392611+ysamardzhiev@users.noreply.github.com>
2024-03-11 22:09:03 +0200

    Update tic_tac_toe.py

--- Python_Advanced/homeworks_and_labs/workshop/tic_tac_toe.py
+++ Python_Advanced/homeworks_and_labs/workshop/tic_tac_toe.py
@@ -65,9 +65,9 @@ def winning_position(game_board, row, col, symbol):
 
 def print_user_interface(first_player):
     print('This is the numeration of the board:')
-    print(f'|  1  |  2  |  3  |')
-    print(f'|  4  |  5  |  6  |')
-    print(f'|  7  |  8  |  9  |')
+    print('|  1  |  2  |  3  |')
+    print('|  4  |  5  |  6  |')
+    print('|  7  |  8  |  9  |')
     print(f'{first_player} starts first!')
 
 
